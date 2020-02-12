d8eb0ba23d8d0658c083728220d1eeeb7e1cbd50
Hasnat <hasib8nh@gmail.com>
2020-02-12 13:41:29 +0600

    Adding Redux to the app and create Root-reducer, user actions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 import Header from './components/header/header.component';
 
@@ -11,34 +13,29 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 import {aut, createUserProfileDocument} from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+
 
 
 class App extends React.Component {
-  
-    constructor(){
-      super();
-      this.state = {
-        currentUser: null
-      };
-    }
 
     unsubscribeFromAuth = null;
 
     componentDidMount (){
+      const { setCurrentUser } = this.props;
+      
       this.unsubscribeFromAuth = aut.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+            setCurrentUser ({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          });
+            });
         });
       }
-      this.setState({currentUser:userAuth});
+      setCurrentUser(userAuth);
       });
     }
 
@@ -53,7 +50,7 @@ class App extends React.Component {
       <div>
         
         <BrowserRouter>
-        <Header currentUser = {this.state.currentUser}/>
+        <Header />
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
@@ -71,5 +68,8 @@ class App extends React.Component {
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+export default connect(null, mapDispatchToProps) (App);
 
