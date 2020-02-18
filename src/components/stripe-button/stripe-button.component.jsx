9cdec1429b6d1141205816a6ec7e07e15369abb8
Hasnat <hasib8nh@gmail.com>
2020-02-18 21:28:18 +0600

    Stripe installation, creating stripe-button folder and implementation in checkout-component

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishablekey = 'pk_test_6kRRkDN0XeDlfUBXJGxlnaGL00tNdZpALC';
+    
+    const onToken = token =>{
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+
+    return (
+        <StripeCheckout
+        label = 'Pay Now'
+        name = 'Online Shopping Bd.'
+        billingAddress
+        shippingAddress
+        image='https://svgshare.com/i/CUz.svg'
+        description= {`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel='Pay Now'
+        token={onToken}
+        stripeKey={publishablekey}
+
+        />
+    );
+
+};
+
+export default StripeCheckoutButton;
