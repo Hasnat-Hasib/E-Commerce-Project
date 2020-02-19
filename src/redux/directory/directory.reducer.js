a67038ef595b96c2e6d0f12b651ce3a37e022c4b
Hasnat <hasib8nh@gmail.com>
2020-02-19 20:35:07 +0600

    Updating homepage product link

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,40 +1,45 @@
 const INITIAL_STATE = {
-    sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3
-        },
-        {
-          title: 'womens',
-          imageUrl: './images/w1.jpg',
-          size: 'large',
-          id: 4
-        },
-        {
-          title: 'mens',
-          imageUrl: './images/hats.png',
-          size: 'large',
-          id: 5
-        }
-      ]
+  sections: [
+    {
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+      id: 1,
+      linkUrl: 'shop/hats'
+    },
+    {
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+      id: 2,
+      linkUrl: 'shop/jackets'
+    },
+    {
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+      id: 3,
+      linkUrl: 'shop/sneakers'
+    },
+    {
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+      size: 'large',
+      id: 4,
+      linkUrl: 'shop/womens'
+    },
+    {
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens'
+    }
+  ]
 };
 
 const directoryReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
-        default:
-            return state;
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
 
 export default directoryReducer;
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -157,48 +157,33 @@ const SHOP_DATA = {
     title: 'Womens',
     routeName: 'womens',
     items: [
+
       {
-        id: 23,
-        name: 'Blue Tanktop',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+        id: 1,
+        name: 'Brown Brim',
+        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
         price: 25
       },
       {
-        id: 24,
-        name: 'Floral Blouse',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-        price: 20
+        id: 2,
+        name: 'Blue Beanie',
+        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+        price: 18
+      },
+      
+      
+      {
+        id: 1,
+        name: 'Brown Brim',
+        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+        price: 25
       },
       {
-        id: 25,
-        name: 'Floral Dress',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        price: 80
+        id: 2,
+        name: 'Blue Beanie',
+        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+        price: 18
       },
-      {
-        id: 26,
-        name: 'Red Dots Dress',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        price: 80
-      },
-      {
-        id: 27,
-        name: 'Striped Sweater',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-        price: 45
-      },
-      {
-        id: 28,
-        name: 'Yellow Track Suit',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-        price: 135
-      },
-      {
-        id: 29,
-        name: 'White Blouse',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-        price: 20
-      }
     ]
   },
   mens: {
