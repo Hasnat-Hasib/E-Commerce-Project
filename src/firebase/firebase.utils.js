dbc71db45326b9157344b7ef37ae994681cee311
Hasnat <hasib8nh@gmail.com>
2020-02-21 19:05:42 +0600

    uploading file with bugs

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -39,6 +39,40 @@ const config = {
     return userRef;
   };
 
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj=> {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+      });
+
+      return await batch.commit();
+    
+  };
+
+
+export const convertCollectionsSnapshotToMap = (collections) =>{
+    const transformedCollection = collections.docs.map(doc=>{
+      const {title, items} = doc.data();
+
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) =>{
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    } , {});
+
+  };
+
   export const aut = firebase.auth();
   export const firestore = firebase.firestore();
 
